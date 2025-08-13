5194c11442dc08d7d44e5c6406af0078985af097
Luizfernando <luizfernando.carrupt@gmail.com>
2025-08-13 15:09:07 -0300

    Fazendo Seletores

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
-
 import { GlobalStyle } from './styles'
+import { store } from './store'
+import { Provider, useSelector } from 'react-redux'
 
 export type Game = {
   id: number
@@ -16,7 +17,6 @@ export type Game = {
 
 function App() {
   const [games, setGames] = useState<Game[]>([])
-  const [carrinho, setCarrinho] = useState<Game[]>([])
 
   useEffect(() => {
     fetch('http://localhost:4000/produtos')
@@ -24,23 +24,23 @@ function App() {
       .then((res) => setGames(res))
   }, [])
 
-  function adicionarAoCarrinho(jogo: Game) {
-    if (carrinho.find((game) => game.id === jogo.id)) {
-      alert('Item já adicionado')
-    } else {
-      setCarrinho([...carrinho, jogo])
-    }
+  function adicionarAoCarrinho() {
+    console.log("Teste")
   }
 
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <div className="container">
-        <Header itensNoCarrinho={carrinho} />
+        <Header />
         <Produtos jogos={games} adicionarAoCarrinho={adicionarAoCarrinho} />
       </div>
-    </>
+    </Provider>
   )
 }
 
 export default App
+function setGames(res: any): any {
+  throw new Error('Function not implemented.')
+}
+
---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import carrinhoReducer from './reducers/carrinho'
+
+export const store = configureStore({
+  reducer: {
+    carrinho: carrinhoReducer
+  }
+})
+
+
+export type RootReducer = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
